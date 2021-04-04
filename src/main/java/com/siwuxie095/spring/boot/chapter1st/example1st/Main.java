e7c32b5559e1bb366ca012a65a1dbceb8f1e33fd
siwuxie095 <834879583@qq.com>
2021-04-04 22:03:31 +0800

    入门；

---
+++ src/main/java/com/siwuxie095/spring/boot/chapter1st/example1st/Main.java
@@ -0,0 +1,31 @@
+package com.siwuxie095.spring.boot.chapter1st.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-04-04 21:53:55
+ */
+public class Main {
+
+    /**
+     * 入门
+     *
+     * Spring Framework 已有十余年的历史了，已成为 Java 应用程序开发框架的事实标准。在如此悠久的
+     * 历史背景下，有人可能会认为 Spring 放慢了脚步，躺在了自己的荣誉簿上，再也做不出什么新鲜的东西，
+     * 或者是让人激动的东西。甚至有人说，Spring 是遗留项目，是时候去看看其他创新的东西了。
+     *
+     * 这些人说得不对。
+     *
+     * Spring 的生态圈里正在出现很多让人激动的新鲜事物，涉及的领域涵盖云计算、大数据、无模式的数据
+     * 持久化、响应式编程以及客户端应用程序开发。
+     *
+     * 在过去的一年多时间里，最让人兴奋、回头率最高、最能改变游戏规则的东西，大概就是 Spring Boot
+     * 了。Spring Boot 提供了一种新的编程范式，能在最小的阻力下开发 Spring 应用程序。有了它，你
+     * 可以更加敏捷地开发 Spring 应用程序，专注于应用程序的功能，不用在 Spring 的配置上多花功夫，
+     * 甚至完全不用配置。实际上，Spring Boot 的一项重要工作就是让 Spring 不再成为你成功路上的绊
+     * 脚石。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
