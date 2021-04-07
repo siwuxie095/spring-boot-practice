d030cc904ef8e574f0880c37c288e863219a5c02
siwuxie095 <834879583@qq.com>
2021-04-07 21:47:05 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/boot/chapter1st/example4th/Main.java
@@ -0,0 +1,22 @@
+package com.siwuxie095.spring.boot.chapter1st.example4th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-04-07 21:43:08
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * Spring Boot 为 Spring 应用程序的开发提供了一种激动人心的新方式，框架本身带来的阻力很小。自动配置
+     * 消除了传统 Spring 应用程序里的很多样板配置；Spring Boot 起步依赖让你能通过库所提供的功能而非名称
+     * 与版本号来指定构建依赖；Spring Boot CLI 将 Spring Boot 的无阻碍开发模型提升到了一个崭新的高度，
+     * 在命令行里就能简单快速地用 Groovy 进行开发；Actuator 让你能深入运行中的应用程序，了解 Spring
+     * Boot 做了什么，是怎么做的。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
