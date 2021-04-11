30e8c3830c1c5c4d740f743d74b164f72b5e6d30
siwuxie095 <834879583@qq.com>
2021-04-11 17:48:18 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/boot/chapter2nd/example5th/Main.java
@@ -0,0 +1,24 @@
+package com.siwuxie095.spring.boot.chapter2nd.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-04-11 17:46:07
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 通过 Spring Boot 的起步依赖和自动配置，你可以更加快速、便捷地开发 Spring 应用程序。起步依赖帮助你
+     * 专注于应用程序需要的功能类型，而非提供该功能的具体库和版本。与此同时，自动配置把你从样板式的配置中解
+     * 放了出来。这些配置在没有 Spring Boot 的 Spring 应用程序里非常常见。
+     *
+     * 虽然自动配置很方便，但在开发 Spring 应用程序时其中的一些用法也有点武断。要是你在配置 Spring 时希望
+     * 或者需要有所不同，该怎么办？后续将会看到如何覆盖 Spring Boot 自动配置，借此达成应用程序的一些目标，
+     * 还有如何运用类似的技术来配置自己的应用程序组件。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
