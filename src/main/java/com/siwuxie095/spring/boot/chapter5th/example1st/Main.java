a6f06fbcc3af83268176264a122c94629f2ede87
siwuxie095 <834879583@qq.com>
2021-04-17 23:28:36 +0800

    Groovy 与 Spring Boot CLI；

---
+++ src/main/java/com/siwuxie095/spring/boot/chapter5th/example1st/Main.java
@@ -0,0 +1,28 @@
+package com.siwuxie095.spring.boot.chapter5th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-04-17 23:25:42
+ */
+public class Main {
+
+    /**
+     * Groovy 与 Spring Boot CLI
+     *
+     * 有些东西真的很适合在一起：花生酱和果酱，Abbott 和 Costello，电闪和雷鸣，牛奶和饼干。每样东西都很棒，
+     * 但搭配起来就更赞了。
+     *
+     * PS：Abbott 和 Costello 两位都是美国的喜剧演员。
+     *
+     * 到目前为止，已经看到了 Spring Boot 带来的不少好东西，包括自动配置和起步依赖。要是再搭配上 Groovy
+     * 的优雅，就能起到一加一大于二的效果。
+     *
+     * 在这里，会了解 Spring Boot CLI。这是一个命令行工具，将强大的 Spring Boot 和 Groovy 结合到一起，
+     * 针对 Spring 应用程序形成了一套简单而又强大的开发工具。为了演示 Spring Boot CLI 的强大之处，这里
+     * 会回到阅读列表应用程序，利用 CLI 的优势，以 Groovy 重写这个应用程序。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
