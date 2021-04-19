e99e441d4c5d1139758b355afd0ad4e210d9841d
siwuxie095 <834879583@qq.com>
2021-04-19 22:55:30 +0800

    在 Spring Boot 中使用 Grails；

---
+++ src/main/java/com/siwuxie095/spring/boot/chapter6th/example1st/Main.java
@@ -0,0 +1,35 @@
+package com.siwuxie095.spring.boot.chapter6th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-04-19 22:50:40
+ */
+public class Main {
+
+    /**
+     * 在 Spring Boot 中使用 Grails
+     *
+     * 小时候，有一个系列电视广告，当中有两个人，一个在吃巧克力条，另一个在吃罐子里的花生酱。经由一些富有喜剧效果
+     * 的小事故，两个人撞到了一起。最后，花生酱和巧克力相结合。
+     *
+     * 一个人说："你把巧克力弄到我的花生酱里了！" 另一个人回答："是你把花生酱弄到我的巧克力上了！"
+     *
+     * 在一开始的尴尬后，两个人都认同花生酱和巧克力结合在一起是件好事。接着，旁白会建议观众试试 Reese 牌的的花生
+     * 酱杯（Peanut Butter Cup）。
+     *
+     * 在 Spring Boot 刚发布时，经常有人问在 Spring Boot 和 Grails 之间该如何选择。两者都构建于 Spring
+     * Framework 之上，都旨在简化应用程序的开发。实际上，它们就像花生酱和巧克力。两个都很好，具体如何选择取
+     * 决于个人爱好。
+     *
+     * 就像之前巧克力和花生酱的争论一样，事实上并不必从中选出一个来。Spring Boot 和 Grails 两个都很好，完全可
+     * 以结合到一起。
+     *
+     * 在这里，会看到 Grails 和 Spring Boot 之间的联系。会先看到 Spring Boot 中 Grails 对象关系映射
+     * （Grails Object Relational Mapping，GORM）和 Groovy 服务器页面（Groovy Server Page， GSP）
+     * 这样的 Grails 特性，还会看到 Grails 3 是如何基于 Spring Boot 重写的。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
