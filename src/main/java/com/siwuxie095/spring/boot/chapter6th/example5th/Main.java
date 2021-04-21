a904bf052931656873fc9186d927dccde99a14f1
siwuxie095 <834879583@qq.com>
2021-04-21 23:09:19 +0800

    小结；

---
+++ src/main/java/com/siwuxie095/spring/boot/chapter6th/example5th/Main.java
@@ -0,0 +1,28 @@
+package com.siwuxie095.spring.boot.chapter6th.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-04-21 23:06:17
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * Grails 和 Spring Boot 都旨在让开发者的生活更简单，大大简化基于 Spring 的开发模型，因此两者
+     * 看起来是互相竞争的框架。但在这里，看到了两者如何结合在一起，综合优势。
+     *
+     * 了解了如何向典型的 Spring Boot 应用程序中添加 GORM 和 GSP 视图，这两个都是知名的 Grails 特
+     * 性。GORM 是 Spring Boot 里一个很受欢迎的特性，能让你直接针对领域模型执行持久化操作，消除了对
+     * 模型仓库的需求。
+     *
+     * 随后认识了 Grails 3，即 Grails 构建于 Spring Boot 之上的最新版本。在开发 Grails 3 应用程
+     * 序时，你也在使用 Spring Boot，可以使用 Spring Boot 的全部特性，包括自动配置。
+     *
+     * 在这里，看到了如何结合 Groovy 和 Spring Boot，消除 Java 语言无法避免的那些代码噪声。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
