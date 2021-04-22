cf8d63758d0782b5d704f9f81a13a6fe0d75a941
siwuxie095 <834879583@qq.com>
2021-04-22 08:44:41 +0800

    深入 Actuator；

---
+++ src/main/java/com/siwuxie095/spring/boot/chapter7th/example1st/Main.java
@@ -0,0 +1,28 @@
+package com.siwuxie095.spring.boot.chapter7th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-04-22 08:41:38
+ */
+public class Main {
+
+    /**
+     * 深入 Actuator
+     *
+     * 你有没有猜过包好的礼物盒里装的是什么东西？你会摇一摇，掂一掂，量一量，你甚至会执着于里面到底有什么。
+     * 但打开盒子那一刻前，你没办法确认里面是什么。
+     *
+     * 运行中的应用程序就像礼物盒。你可以刺探它，作出合理的推测，猜测它的运行情况。但如何了解真实的情况呢？
+     * 有没有一种办法能让你深入应用程序内部一窥究竟，了解它的行为，检查它的健康状况，甚至触发一些操作来影
+     * 响应用程序呢？
+     *
+     * 在这里，将了解 Spring Boot 的 Actuator。它提供了很多生产级的特性，比如监控和度量 Spring Boot
+     * 应用程序。Actuator 的这些特性可以通过众多 REST 端点、远程 shell 和 JMX 获得。
+     *
+     * 后续会先来看看 Actuator 的 REST 端点，这种最为人所熟知的使用方式提供了最完整的功能。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
